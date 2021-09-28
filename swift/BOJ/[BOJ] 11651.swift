7f0a97fddb36b7c64d31d25aed7af75ced12ed41
Jason9789 <lifebook0809@gmail.com>
2021-09-28 19:36:11 +0900

    sol: BOJ 11650, 11651

---
+++ swift/BOJ/[BOJ] 11651.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+var n: Int = Int(readLine()!)!
+
+var points = [[Int]]()
+
+for _ in 0..<n {
+    points.append( readLine()!.split(separator: " ").map { Int(String($0))! } )
+}
+
+
+points.sorted { lhs, rhs -> Bool in
+
+    return lhs[1] < rhs[1] || (lhs[1] == rhs[1] && lhs[0] < rhs[0])
+    
+}.forEach { point in
+    print("\(point[0]) \(point[1])")
+    
+}
