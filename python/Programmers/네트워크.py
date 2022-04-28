d476e925c94bda0db0e65c4e43a53aed1aba325e
Jason9789 <lifebook0809@gmail.com>
2022-04-28 15:16:58 +0900

    sol: [Tree] 543, [Graph] 네트워크, 타깃넘버

---
+++ python/Programmers/네트워크.py
@@ -0,0 +1,17 @@
+def solution(n, computers):
+
+    def dfs(i):
+        visited[i] = 1
+        for a in range(n):
+            if computers[i][a] and not visited[a]:
+                dfs(a)
+
+    answer = 0
+    visited = [0 for i in range(len(computers))]
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+
+    return answer
