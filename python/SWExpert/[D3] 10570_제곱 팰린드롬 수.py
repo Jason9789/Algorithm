b2a0d455528c6c40bad4de8276c7d788eaf6cf66
Jason9789 <lifebook0809@gmail.com>
2022-05-22 21:46:35 +0900

    sol: [D3] 10570

---
+++ python/SWExpert/[D3] 10570_제곱 팰린드롬 수.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    A, B = map(int, input().split())
+    cnt = 0
+
+    for i in range(A, B+1):
+        C = i ** (1 / 2)
+
+        if C == int(C):
+            i = str(i)
+            C = str(int(C))
+
+            if i == i[::-1] and C == C[::-1]:
+                cnt += 1
+
+    print(f'#{tc} {cnt}')
