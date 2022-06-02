eeddbb438f4fb8e610bd0173323784c6c5e9acd2
Jason9789 <lifebook0809@gmail.com>
2022-06-02 14:34:45 +0900

    sol: [BOJ] BFS 24444, 24445

---
+++ python/BOJ/[BOJ] 24445_너비우선탐색2.py
@@ -0,0 +1,39 @@
+from collections import deque
+import sys
+
+
+def bfs(r):
+    cnt = 1
+    visited[r] = cnt
+
+    queue = deque()
+    queue.append(r)
+
+    while queue:
+        node = queue.popleft()
+
+        for i in graph[node]:
+            if visited[i] == 0:
+                cnt += 1
+                queue.append(i)
+                visited[i] = cnt
+
+
+n, m, r = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+
+# 그래프 만들기
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().split())
+
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in graph:
+    i.sort(reverse=True)
+
+bfs(r)
+
+for i in range(1, n+1):
+    print(visited[i])
